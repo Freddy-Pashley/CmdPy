be6970521e201c38133f4e4291fb0aefb4355385
Fred <68379946+Fredfredster@users.noreply.github.com>
2021-01-13 15:46:43 +0000

    Update 1.0.2
    Added commands: VERSION, CLEAR.
    Set up the help command with the necessary information.

--- CmdPy.py
+++ CmdPy.py
@@ -4,28 +4,34 @@ Product details.
 # Author: Fred Pashley
 # License: MIT
 # Date of creation: 12/01/2021
-# Current Version: 1.0.1
+# Current Version: 1.0.2
 
 """
 Product version.
 This is the current version, and not always the latest.
 """
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 
 """
 All imports.
 Imports are prefered to be whole modules/packages, not sub-modules.
 This is to prevent errors with code and text later.
+System installes any modules that are not available on local machine.
 """
 import os
 import sys
 import time
+try:
+    import pytz
+except ImportError:
+    os.system('pip install pytz')
 
 """
 Gets current directory of CmdPy file.
 """
 CURRENTDIR = str(os.path.dirname(os.path.realpath(__file__)))
-COMMANDS = ['HELP', 'EXIT']
+COMMANDS = ['HELP', 'EXIT', 'VERSION', 'CLEAR']
+COMMANDS_SORTED = sorted(COMMANDS)
 
 """
 Prints welcome and copyright.
@@ -44,39 +50,75 @@ while True:
     raw_cmd = input(f'\n{CURRENTDIR}>')
 
     """
-    Splits the entire string for arguments.
+    Splits the entire string to check for arguments into a list.
     """
     i = raw_cmd.split()
 
     """
-    Checks if the command sent was empty.
-    If empty, passes and repeats.
-    If there are arguments, saves to a list.
+    Checks if there are arguments specified.
+    Or checks if no arguments specified.
+    Sets ARGUMENTS list respectively.
     """
-    if i == []:
-        pass
-    elif len(i) > 1:
+    if len(i) > 1:
         ARGUMENTS = []
         for item in i[1:]:
-            ARGUMENTS.append(item)
+            ARGUMENTS.append(item.upper())
+    elif len(i) == 1:
+        ARGUMENTS = []
+    elif len(i) == 0:
+        pass
 
     """
     Gets command not arguments.
     """
-    COMMAND = str(i[0]).upper()
+    if len(i) > 0:
+        COMMAND = str(i[0]).upper()
+    else:
+        pass
 
     """
     Checks for command.
     """
     if COMMAND in COMMANDS:
         if COMMAND == 'HELP':
-            print('For more information on a command, type HELP command-name.\n')
-            for item in COMMANDS:
-                print(item.upper())
-            print('\nYou can also check the online documentation for a more detailed\nexplanation of a command, or if you are having any issues.')
+            if ARGUMENTS == []:
+                print('For more information on a command, type HELP command-name.\n')
+                for item in COMMANDS_SORTED:
+                    print(item.upper())
+                print('\nYou can also check the online documentation for a more detailed\nexplanation of a command, or if you are having any issues.')
+            else:
+                """
+                Gets the first arguments and assigns it as the command the user needs help with.
+                This is already in upper-form, as when the ARGUMENTS list is created each argument is appended as upper.
+                """
+                help_command = ARGUMENTS[0]
+                
+                """
+                Checks for command.
+                """
+                if help_command == 'HELP':
+                    print('Provides help information for CmdPy commands.\n\nSyntax: HELP command-name')
+                
+                elif help_command == 'EXIT':
+                    print('Exits CmdPy.\n\nSyntax: EXIT')
+
+                elif help_command == 'VERSION':
+                    print('Provides the version of CmdPy you are using.\n\nSyntax: VERSION')
+
+                elif help_command == 'CLEAR':
+                    print('Clears the screen.\n\nSyntax: CLEAR')
 
         elif COMMAND == 'EXIT':
             quit()
+
+        elif COMMAND == 'VERSION':
+            print(VERSION)
+
+        elif COMMAND == 'CLEAR':
+            if os.name == 'nt':
+                os.system('cls')
+            else:
+                os.system('clear')
 
     else:
         print(f"'{COMMAND.lower()}' is not recognised as a command.\nIf this is unexpected, please check the online documentation for help.")
