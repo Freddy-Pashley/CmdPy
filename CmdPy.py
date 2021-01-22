433a77184ef2c1dd6299c1783e09ab71d32c2f2e
Fred <68379946+Fredfredster@users.noreply.github.com>
2021-01-22 12:03:43 +0000

    1.0.0
    Ping command and stability fixes

--- CmdPy.py
+++ CmdPy.py
@@ -4,20 +4,28 @@ from datetime import datetime as dt
 
 VERSION = '1.0.0'
 
-print('CmdPy [Version {}]\n(c) 2021 Fred Pashley. All rights reserved.\n'.format(VERSION))
+print('CmdPy [Version {}]\n(c) 2021 Fred Pashley. All rights reserved.'.format(VERSION))
 
 class Command:
     def __init__(self, name, arguments):
         self.name = name
         self.arguments = list(arguments)
 
-COMMANDS = ['help']
+def invalid_line(command):
+    print("When using the '{}' command, you provided an invalid command line.".format(command))
 
-HelpCommand = Command('help', ['help'])
+
+
+HelpCommand = Command('help', ['help', 'ping'])
+PingCommand = Command('ping', [])
+
+COMMANDS = []
+for item in HelpCommand.arguments:
+    COMMANDS.append(item)
 
 while True:
 
-    rawUserCommand = input('{}>'.format(str(os.path.dirname(os.path.realpath(__file__)))))
+    rawUserCommand = input('\n{}>'.format(str(os.path.dirname(os.path.realpath(__file__)))))
 
     UserArguments = []
     if len(rawUserCommand.split()) > 1:
@@ -29,7 +37,22 @@ while True:
 
     if UserCommand in COMMANDS:
         if UserCommand == HelpCommand.name:
-            print('help')
+            if UserArguments == []:
+                print('For more information on a specific command, type HELP command-name')
+                for item in COMMANDS:
+                    print(item.upper())
+            elif UserArguments[0] in HelpCommand.arguments:
+                if UserArguments[0] == 'help':
+                    print('Basic help command for CmdPy.\n\nSyntax: HELP command-name\n\nLeave command-name blank for list of commands,\nor specify a command for more detail.')
+                elif UserArguments[0] == 'ping':
+                    print('Pings the specified host. Returns the average time in milli-seconds.\n\nSyntax: PING host\n\nHost is a required field.')
+            else:
+                invalid_line('help')
+        elif UserCommand == PingCommand.name:
+            if UserArguments == []:
+                invalid_line('ping')
+            else:
+                print(os.system(f'ping {UserArguments[0]}'))
     elif UserCommand == '':
         pass
     else:
